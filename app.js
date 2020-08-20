eb0d7b6cb97a78a13898b26e7ca15d9fbb41345e
devCynosure <sharanprakash.sfc@gmail.com>
2020-08-21 00:50:32 +0530

    Complete Which day is current day (swith statement)

--- app.js
+++ app.js
@@ -10,17 +10,37 @@ app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay();
   var whichDay = "";
-  if (currentDay === 0 || currentDay === 6) {
-    whichDay = "Weekend";
-  } else {
-    whichDay = "weekday";
+  console.log(currentDay);
+  switch (currentDay) {
+    case 0:
+      whichDay = "Sunday";
+      break;
+    case 1:
+      whichDay = "Monday";
+      break;
+    case 2:
+      whichDay = "Tuesday";
+      break;
+    case 3:
+      whichDay = "Wednessday";
+      break;
+    case 4:
+      whichDay = "Thusday";
+      break;
+    case 5:
+      whichDay = "Friday";
+      break;
+    case 6:
+      whichDay = "Saturday";
+      break;
+    default:
+      whichDay = "Error Occured!";
   }
-  res.render("list", {
+  res.render('list', {
     dayIs: whichDay
   });
 
 });
-
 app.listen(port, function() {
   console.log("server running on port " + port);
 });
