63f220b96c318c8cd4a1d5bfc12a407ac32b12c1
devCynosure <sharanprakash.sfc@gmail.com>
2020-08-21 00:22:14 +0530

    Complete ejs shows weekday or weekend

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  var today = new Date();
+  var currentDay = today.getDay();
+  var whichDay = "";
+  if (currentDay === 0 || currentDay === 6) {
+    whichDay = "Weekend";
+  } else {
+    whichDay = "weekday";
+  }
+  res.render("list", {
+    dayIs: whichDay
+  });
+
 });
 
 app.listen(port, function() {
