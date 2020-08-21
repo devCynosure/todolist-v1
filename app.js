dca74faaa6e0dca564f38835aa720ca91a9cdc05
devCynosure <sharanprakash.sfc@gmail.com>
2020-08-21 15:47:01 +0530

    Develop todolist add options to add to list and loop the list

--- app.js
+++ app.js
@@ -3,29 +3,40 @@ const bodyParser = require('body-parser');
 
 const port = 3000;
 const app = express();
+var items = [];
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  var options ={
-    weekday :'long',
-    day : 'numeric',
-    month : 'long',
-    year : 'numeric'
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
   }
   var whichDay = today.toLocaleDateString("en-IN", options);
 
   console.log(currentDay);
   console.log(whichDay);
 
-
   res.render('list', {
-    dayIs: whichDay
+    dayIs: whichDay,
+    itemslist: items
   });
+});
+app.post("/", function(req, res) {
+
+  var item = req.body.itemToDoListTextbox;
+  items.push(item);
+  res.redirect('/');
 
 });
+
 app.listen(port, function() {
   console.log("server running on port " + port);
 });
